f05dd663e487bb044cf47bf8ee49509b299bd174
Fang YuTong <fang.22@ichat.sp.edu.sg>
2023-05-04 14:42:18 +0800

    whatever kill me

--- Calculate_BMI.py
+++ Calculate_BMI.py
@@ -8,9 +8,12 @@ def calculate_bmi(height,weight):
 
     if BMI<18.5:
         print("Under Weight")
+        return -1
     elif 18.5<=BMI<=25.0:
         print("Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 
 calculate_bmi(weight=57,height=1.73)
